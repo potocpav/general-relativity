7f6ab69404f4ddff935defa154cafc60eb7ba038
Pavel Potoček <pavelpotocek@gmail.com>
2023-10-20 17:34:11 +0200

    Add sprite generator script

---
+++ models/gen_sprites.sh
@@ -0,0 +1,3 @@
+convert -append render/asteroid/0*.png render/asteroid.png
+convert -append render/rocket/0*.png render/rocket-firing/0*.png render/rocket.png
+convert +append render/asteroid.png render/rocket.png sprites.png
